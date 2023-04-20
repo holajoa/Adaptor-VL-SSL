67bdf6bfb75e26ed584fd1199f1becf98a9cb1a9
Jiuming Qin <77429740+holajoa@users.noreply.github.com>
2023-04-20 12:48:23 +0100

    fusioner implementation

--- models.py
+++ models.py
@@ -2,13 +2,17 @@ import torch
 import torch.nn as nn
 from typing import List, Union, Tuple, Dict, Optional
 
-from transformers import BertConfig
+from transformers import BertConfig, PretrainedConfig, AutoConfig
+from transformers import VisionTextDualEncoderModel
 from transformers.models.bert.modeling_bert import BertEncoder, BertPooler
 from transformers.modeling_outputs import (
     BaseModelOutputWithPastAndCrossAttentions, 
     BaseModelOutputWithPoolingAndCrossAttentions, 
 )
 from transformers.modeling_utils import ModuleUtilsMixin
+from transformers.models.clip.modeling_clip import CLIPOutput
+
+from transformers.models.clip.modeling_clip import clip_loss
 
 
 class FusionModule(nn.Module, ModuleUtilsMixin):
@@ -20,7 +24,6 @@ class FusionModule(nn.Module, ModuleUtilsMixin):
         self.encoder = BertEncoder(self.config)
         self.pooler = BertPooler(self.config)
 
-    
     def forward(
         self, 
         text_embeds:torch.Tensor, 
@@ -109,3 +112,83 @@ class FusionModule(nn.Module, ModuleUtilsMixin):
             attentions=encoder_outputs.attentions,
             cross_attentions=encoder_outputs.cross_attentions,
         )
+
+class Fusion(nn.Module):
+    def __init__(
+        self, 
+        text_pretrained_model_name_or_path:str, 
+        vision_pretrained_model_name_or_path:str,
+        config:Optional[BertConfig]=None, 
+    ):
+        super(Fusion, self).__init__()
+        
+        self.encoder = VisionTextDualEncoderModel.from_vision_text_pretrained(
+            vision_pretrained_model_name_or_path,  
+            text_pretrained_model_name_or_path,
+        )
+        self.fusion_module = FusionModule(config)
+        
+        
+    def forward(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        pixel_values: Optional[torch.FloatTensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        return_loss: Optional[bool] = None,
+        token_type_ids: Optional[torch.LongTensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple[torch.Tensor], CLIPOutput]:
+        
+        encoder_outputs: CLIPOutput = self.encoder(
+            input_ids=input_ids, 
+            pixel_values=pixel_values,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            return_loss=False,
+            token_type_ids=token_type_ids,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=True, 
+        )
+        
+        text_embeds_raw = encoder_outputs.text_model_output.last_hidden_state
+        # text_pooler_output = encoder_outputs.text_model_output.pooler_output
+        image_embeds_raw = encoder_outputs.vision_model_output.last_hidden_state
+        # vision_pooler_output = encoder_outputs.vision_model_output.pooler_output
+
+        text_seq_len, image_seq_len = text_embeds_raw.shape[1], image_embeds_raw.shape[1]
+        outputs = self.fusion_module(text_embeds_raw, image_embeds_raw)
+        
+        text_embeds = outputs.last_hidden_state[:, :text_seq_len, :]
+        image_embeds = outputs.last_hidden_state[:, text_seq_len:, :]
+        
+        # normalized features
+        image_embeds = image_embeds / image_embeds.norm(dim=-1, keepdim=True)
+        text_embeds = text_embeds / text_embeds.norm(dim=-1, keepdim=True)
+        
+        # cosine similarity as logits
+        logit_scale = self.encoder.logit_scale.exp()
+        logits_per_text = torch.matmul(text_embeds[:, 0, :], image_embeds[:, 0, :].t()) * logit_scale
+        logits_per_image = logits_per_text.T
+        
+        loss = None
+        if return_loss:
+            loss = clip_loss(logits_per_text)
+        
+        if not return_dict:
+            output = (logits_per_image, logits_per_text, text_embeds, image_embeds)
+            return ((loss,) + output) if loss is not None else output
+        
+        return CLIPOutput(
+            loss=loss,
+            logits_per_image=logits_per_image,
+            logits_per_text=logits_per_text,
+            text_embeds=text_embeds,
+            image_embeds=image_embeds,
+            # text_model_output=text_outputs,
+            # vision_model_output=vision_outputs,
+        )
+        
