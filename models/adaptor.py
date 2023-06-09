e86819b2522ba59835ce321f08cdae78fd264013
Jiuming Qin <holajoa@outlook.com>
2023-06-09 10:52:28 +0100

    change adaptor to cross-attention, and can process full sequences

--- models/adaptor.py
+++ models/adaptor.py
@@ -1,16 +1,15 @@
 from typing import List, Union, Tuple, Dict, Optional
 
-import torch 
+import torch
 import torch.nn as nn
+from torch.nn.modules.transformer import _get_activation_fn
 
-import pytorch_lightning as pl
+from pytorch_lightning import LightningModule
 
 from transformers import BertConfig
 from transformers.models.bert.modeling_bert import BertEncoder, BertPooler
-from transformers.modeling_outputs import (
-    BaseModelOutputWithPastAndCrossAttentions, 
-    BaseModelOutputWithPoolingAndCrossAttentions, 
-)
+from transformers.modeling_outputs import BaseModelOutputWithPooling
+
 from transformers.modeling_utils import ModuleUtilsMixin
 from transformers.models.clip.modeling_clip import CLIPOutput
 from transformers.models.clip.modeling_clip import clip_loss
@@ -22,121 +21,106 @@ from tqdm import tqdm
 import sys
 
 
+class TransformerEncoderLayerWithCrossAttention(nn.Module):
+    r"""TransformerEncoderLayer is made up of self-attn and feedforward network.
+    This standard encoder layer is based on the paper "Attention Is All You Need".
+    Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion Jones, Aidan N Gomez,
+    Lukasz Kaiser, and Illia Polosukhin. 2017. Attention is all you need. In Advances in
+    Neural Information Processing Systems, pages 6000-6010. Users may modify or implement
+    in a different way during application.
+
+    Args:
+        d_model: the number of expected features in the input (required).
+        nhead: the number of heads in the multiheadattention models (required).
+        dim_feedforward: the dimension of the feedforward network model (default=2048).
+        dropout: the dropout value (default=0.1).
+        activation: the activation function of intermediate layer, relu or gelu (default=relu).
+
+    Examples::
+        >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
+        >>> src = torch.rand(10, 32, 512)
+        >>> out = encoder_layer(src)
+    """
+
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation="relu"):
+        super(TransformerEncoderLayerWithCrossAttention, self).__init__()
+        self.attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        # Implementation of Feedforward model
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
+
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.activation = _get_activation_fn(activation)
+
+    def forward(self, src, query=None, src_mask=None, src_key_padding_mask=None, run_cross_attn=False):
+            r"""Pass the input through the encoder layer.
+
+            Args:
+                src: the sequnce to the encoder layer (required).
+                src_mask: the mask for the src sequence (optional).
+                src_key_padding_mask: the mask for the src keys per batch (optional).
+
+            Shape:
+                see the docs in Transformer class.
+            """
+            if run_cross_attn:
+                assert query is not None, "Must provide a query vector"
+                src2 = self.attn(query, src, src, attn_mask=src_mask,
+                                key_padding_mask=src_key_padding_mask)[0]
+            else:   # run self attention
+                src2 = self.attn(src, src, src, attn_mask=src_mask,
+                                key_padding_mask=src_key_padding_mask)[0]
+            src = src + self.dropout1(src2)
+            src = self.norm1(src)
+            if hasattr(self, "activation"):
+                src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
+            else:  # for backward compatibility
+                src2 = self.linear2(self.dropout(F.relu(self.linear1(src))))
+            src = src + self.dropout2(src2)
+            src = self.norm2(src)
+            return src
+
+
 class AdaptorModule(nn.Module, ModuleUtilsMixin):
     def __init__(
-        self, 
-        config:Optional[BertConfig]=None, 
-        num_hidden_layers:int=2, 
+        self,
+        embed_dim:int=768,
+        num_heads:int=12,
     ):
         super(AdaptorModule, self).__init__()
-        if config is not None:
-            self.config = config 
-        else:
-            self.config = BertConfig(num_hidden_layers=num_hidden_layers)
-        
-        self.embeddings = lambda i, t: torch.stack([i, t], dim=1)
-        self.encoder = BertEncoder(self.config)
-        self.pooler = BertPooler(self.config)
+        self.encoder = TransformerEncoderLayerWithCrossAttention(embed_dim, num_heads)
+
 
     def forward(
-        self, 
-        image_embeds:torch.Tensor, 
-        text_embeds:Optional[torch.Tensor]=None, 
-        attention_mask: Optional[torch.FloatTensor] = None,
-        head_mask: Optional[torch.FloatTensor] = None,
-        encoder_hidden_states: Optional[torch.FloatTensor] = None,
-        encoder_attention_mask: Optional[torch.FloatTensor] = None,
-        past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
-        use_cache: Optional[bool] = None,
-        output_attentions: Optional[bool] = False,
-        output_hidden_states: Optional[bool] = False,
-        return_dict: Optional[bool] = True,
-        **kwargs, 
-    ) -> Union[Tuple[torch.Tensor], BaseModelOutputWithPastAndCrossAttentions]:
-        
-        
+        self,
+        image_embeds:torch.Tensor,
+        text_embeds:Optional[torch.Tensor]=None,
+        **kwargs,
+    ) -> Union[Tuple[torch.Tensor], torch.Tensor]:
+
         # Optional text embedding inputs
         if text_embeds is not None:
             assert text_embeds.device == image_embeds.device, "text and image embeddings must be on the same device"
-            inputs_embeds = self.embeddings(image_embeds, text_embeds)
+            ## TODO: Run through cross-attention
+            image_encoder_outputs = self.encoder(src=image_embeds, query=text_embeds, run_cross_attn=True)
+            text_encoder_outputs = self.encoder(src=text_embeds, query=image_embeds, run_cross_attn=True)
+            return image_encoder_outputs, text_encoder_outputs
         else:
-            inputs_embeds = image_embeds
-        if len(inputs_embeds.shape) == 2:
-            inputs_embeds = inputs_embeds.unsqueeze(1)
-        
-        # Copied from transformers/models/bert/modeling_bert.py, BertModel.forward()
-        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
-        output_hidden_states = (
-            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
-        )
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-        
-        use_cache = False
-        
-        device = inputs_embeds.device
-        
-        input_shape = inputs_embeds.size()[:-1]
-        batch_size, seq_length = input_shape
-        
-        past_key_values_length = past_key_values[0][0].shape[2] if past_key_values is not None else 0
-        
-        if attention_mask is None:
-            attention_mask = torch.ones(((batch_size, seq_length + past_key_values_length)), device=device)
-        
-        # We can provide a self-attention mask of dimensions [batch_size, from_seq_length, to_seq_length]
-        # ourselves in which case we just need to make it broadcastable to all heads.
-        extended_attention_mask: torch.Tensor = self.get_extended_attention_mask(attention_mask, input_shape)
-        
-        # If a 2D or 3D attention mask is provided for the cross-attention
-        # we need to make broadcastable to [batch_size, num_heads, seq_length, seq_length]
-        if self.config.is_decoder and encoder_hidden_states is not None:
-            encoder_batch_size, encoder_sequence_length, _ = encoder_hidden_states.size()
-            encoder_hidden_shape = (encoder_batch_size, encoder_sequence_length)
-            if encoder_attention_mask is None:
-                encoder_attention_mask = torch.ones(encoder_hidden_shape, device=device)
-            encoder_extended_attention_mask = self.invert_attention_mask(encoder_attention_mask)
-        else:
-            encoder_extended_attention_mask = None
-            
-        # Prepare head mask if needed
-        # 1.0 in head_mask indicate we keep the head
-        # attention_probs has shape bsz x n_heads x N x N
-        # input head_mask has shape [num_heads] or [num_hidden_layers x num_heads]
-        # and head_mask is converted to shape [num_hidden_layers x batch x num_heads x seq_length x seq_length]
-        head_mask = self.get_head_mask(head_mask, self.config.num_hidden_layers)
-        
-        encoder_outputs = self.encoder(
-            inputs_embeds,   # concatenated text and image embeddings
-            attention_mask=extended_attention_mask,
-            head_mask=head_mask,
-            encoder_hidden_states=encoder_hidden_states,
-            encoder_attention_mask=encoder_extended_attention_mask,
-            past_key_values=past_key_values,
-            use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-        
-        sequence_output = encoder_outputs[0]
-        pooled_output = self.pooler(sequence_output) # if self.pooler is not None else None
+            ## TODO: Run through self-attention
+            encoder_outputs = self.encoder(src=image_embeds, run_cross_attn=False)
 
-        if not return_dict:
-            return (sequence_output, pooled_output) + encoder_outputs[1:]
-        
-        return BaseModelOutputWithPoolingAndCrossAttentions(
-            last_hidden_state=sequence_output,
-            pooler_output=pooled_output,
-            past_key_values=encoder_outputs.past_key_values,
-            hidden_states=encoder_outputs.hidden_states,
-            attentions=encoder_outputs.attentions,
-            cross_attentions=encoder_outputs.cross_attentions,
-        )
+            return encoder_outputs
+
 
 class Project(nn.Module):
     def __init__(
-        self, 
-        text_embed_dim:int,  
+        self,
+        text_embed_dim:int,
         vision_embed_dim:int,
         projection_dim:int=512,
     ):
@@ -151,113 +135,123 @@ class Project(nn.Module):
         self,
         image_embeds_:torch.Tensor,
         text_embeds_:Optional[torch.Tensor]=None,
-    ):  
-        
+    ):
+
         image_embeds = self.visual_projection(image_embeds_)
         image_embeds = image_embeds / image_embeds.norm(dim=-1, keepdim=True)  # normalized features
-        
+
         if text_embeds_ is not None:
             text_embeds = self.text_projection(text_embeds_)
             text_embeds = text_embeds / text_embeds.norm(dim=-1, keepdim=True)  # normalized features
             return image_embeds, text_embeds
-        
+
         return image_embeds
 
 
-class Adaptor(pl.LightningModule):
+class Adaptor(LightningModule):
     def __init__(
-        self, 
-        adaptor_config:Optional[BertConfig]=None, 
-        text_output_dim:int=768, 
+        self,
+        adaptor_config:Optional[BertConfig]=None,
+        text_output_dim:int=768,
         vision_output_dim:Optional[int]=None,  # ignored if vision_model_type is huggingface
         logit_scale_init_value:float=2.6592,  # logit_scale = 1 / temperature
         projection_dim:int=512,
-        num_hidden_layers:int=2,
+        # num_hidden_layers:int=2,
         lr:float=1e-4,
     ):
         super(Adaptor, self).__init__()
-        
+
         self.projection = Project(
-            text_embed_dim=text_output_dim, 
-            vision_embed_dim=vision_output_dim, 
-            projection_dim=projection_dim, 
+            text_embed_dim=text_output_dim,
+            vision_embed_dim=vision_output_dim,
+            projection_dim=projection_dim,
         )
-        self.adaptor_module = AdaptorModule(adaptor_config, num_hidden_layers)
+        self.adaptor_module = AdaptorModule(embed_dim=projection_dim)
         self.vision_output_dim = vision_output_dim
         self.text_output_dim = text_output_dim
         self.projection_dim = projection_dim
-                
+
         self.logit_scale_init_value = logit_scale_init_value
         self.logit_scale = nn.Parameter(torch.ones([]) * logit_scale_init_value)
-        
+
         self.lr = lr
         self.save_hyperparameters()
-        
+
     def forward(
         self,
-        image_embeds_raw:torch.FloatTensor, 
-        text_embeds_raw:Optional[torch.FloatTensor]=None, 
+        image_embeds_raw:torch.FloatTensor,
+        text_embeds_raw:Optional[torch.FloatTensor]=None,
         return_loss:Optional[bool]=True,
         return_dict:Optional[bool]=True,
-        **kwargs, 
+        **kwargs,
     ) -> Union[Tuple[torch.Tensor], torch.Tensor, CLIPOutput]:
-        
-        assert len(image_embeds_raw.shape) == 2
-        
+
         if text_embeds_raw is not None:
-            assert len(text_embeds_raw.shape) == 2
-        
-            image_embeds, text_embeds = self.projection(image_embeds_raw, text_embeds_raw)
-            outputs = self.adaptor_module(image_embeds, text_embeds)
-            
-            image_embeds = outputs.last_hidden_state[:, 0, :]
-            text_embeds = outputs.last_hidden_state[:, 1, :]
-            
-            # normalized features
+            image_embeds_full, text_embeds_full = self.projection(image_embeds_raw, text_embeds_raw)
+            image_embeds_full, text_embeds_full = self.adaptor_module(image_embeds_full, text_embeds_full)  # bz x seq_len x 768 during pretraining
+
+            if len(image_embeds_full.shape) == 2:
+                 image_embeds_full = image_embeds_full.unsqueeze(1)
+            if len(text_embeds_full.shape) == 2:
+                 text_embeds_full = text_embeds_full.unsqueeze(1)
+            image_embeds, text_embeds = image_embeds_full[:, 0, :], text_embeds_full[:, 0, :]
+
+            # normalized features - is this necessary for full embeddings?
+            image_embeds_full = image_embeds_full / image_embeds_full.norm(dim=-1, keepdim=True)
+            text_embeds_full = text_embeds_full / text_embeds_full.norm(dim=-1, keepdim=True)
+
             image_embeds = image_embeds / image_embeds.norm(dim=-1, keepdim=True)
             text_embeds = text_embeds / text_embeds.norm(dim=-1, keepdim=True)
-            
+
             # cosine similarity as logits
             logit_scale = self.logit_scale.exp()
             logits_per_text = torch.matmul(text_embeds, image_embeds.t()) * logit_scale   # [batch_size, batch_size]
             logits_per_image = logits_per_text.T
-            
+
             loss = None
             if return_loss:
                 loss = clip_loss(logits_per_text)
-            
+
             if not return_dict:
                 output = (logits_per_image, logits_per_text, text_embeds, image_embeds)
                 return ((loss,) + output) if loss is not None else output
-            
+
             return CLIPOutput(
                 loss=loss,
                 logits_per_image=logits_per_image,
                 logits_per_text=logits_per_text,
                 text_embeds=text_embeds,
                 image_embeds=image_embeds,
+                text_model_outputs=BaseModelOutputWithPooling(
+                    last_hidden_state=text_embeds_full,
+                    pooler_output=text_embeds,
+                ),
+                image_model_outputs=BaseModelOutputWithPooling(
+                    last_hidden_state=image_embeds_full,
+                    pooler_output=image_embeds,
+                ),
             )
-        
+
         else:   # text_embeds is none
-            image_embeds = self.projection(image_embeds_raw)
-            outputs = self.adaptor_module(image_embeds)
-            image_embeds = outputs.last_hidden_state[:, 0, :]
-            image_embeds = image_embeds / image_embeds.norm(dim=-1, keepdim=True)
-            
-            return image_embeds  # ignore return_dict and return_loss
-    
+            image_embeds_full = self.projection(image_embeds_raw)
+            image_embeds_full = self.adaptor_module(image_embeds_full)
+            image_embeds_full = image_embeds_full / image_embeds_full.norm(dim=-1, keepdim=True)
+
+            return image_embeds_full  # ignore return_dict and return_loss
+
+
     def training_step(self, batch, batch_idx):
         outputs = self(**batch)
         loss = outputs.loss
         self.log('train_loss', loss, prog_bar=True, logger=True)
         self.lr_schedulers().step()
         return loss
-    
+
     def _shared_eval(self, batch, batch_idx, prefix):
         outputs = self(**batch)
         loss = outputs.loss
         self.log(f'{prefix}_loss', loss, prog_bar=True, logger=True)
-        
+
     def validation_step(self, batch, batch_idx):
         self._shared_eval(batch, batch_idx, "val")
 
@@ -267,16 +261,15 @@ class Adaptor(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = AdamW(self.parameters(), lr=self.lr, weight_decay=0.05)
         lr_schedule = CosineAnnealingWarmRestarts(
-            optimizer=optimizer, 
-            T_0=int(self.training_steps*0.4), 
-            T_mult=1, 
-            eta_min=1e-8, 
+            optimizer=optimizer,
+            T_0=int(self.training_steps*0.4),
+            T_mult=1,
+            eta_min=1e-8,
         )
         return {'optimizer':optimizer, 'lr_scheduler':lr_schedule}
-    
+
     def get_progress_bar_dict(self):
         # don't show the version number
         items = super().get_progress_bar_dict()
         items.pop("v_num", None)
         return items
-        
